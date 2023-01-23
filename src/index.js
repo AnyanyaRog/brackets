0a803ea131fb7eedec325d5cbf188946eb8860c2
AnyanyaRog <anyaroganova2002@yandex.ru>
2023-01-23 19:14:03 +0300

    docks: JS brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let arr3 = []
+  let n = 1;
+  let z = -1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == '(' || str[i] == '{' || str[i] == '[') {
+      arr3.push(str[i])
+      z += 1;
+    } else if (str[i] == '|' && n) {
+      n = 0
+      arr3.push(str[i])
+      z += 1;
+    }
+
+    else {
+      if (str[i] == ')' && arr3[z] == "(") {
+
+        z -= 1;
+        arr3.pop()
+
+      } else if (str[i] == ']' && arr3[z] == "[") {
+        z -= 1;
+        arr3.pop()
+      } else if (str[i] == '}' && arr3[z] == "{") {
+        z -= 1;
+        arr3.pop()
+      }
+
+      else if (str[i] == '|' && arr3[z] == "|" && n == 0) {
+
+        z -= 1;
+        arr3.pop();
+        n = 1
+      }
+
+    }
+    if (str[i] >= '0' && str[i] <= '9') {
+      if (str.length % 33 == 0) {
+        return true;
+      } else
+        return false;
+    }
+
+  }
+
+  if (arr3.length == 0 && (str.length % 2 == 0)) return true
+  else return false
 }
